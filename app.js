d2b8bc3955e2f4ca7061cf45b4d71405b8b78be2
CozzyBro2 <gojinhan2@gmail.com>
2023-06-19 14:43:12 -0400

    experiment

--- app.js
+++ app.js
@@ -8,27 +8,23 @@ const port = ( process.env.PORT || 5530 )
 const mediaPath = path.join(__dirname, '/media/')
 const pagePath = path.join(__dirname, '/pages/')
 
-if (!mediaPath) {
-    throw new Error('Missing media folder.')
-}
-
 app.get('/', (_, res) => {
     res.sendFile(pagePath + 'home.html')
 })
 
-app.get('/:file', (req, res) => {
+app.get('/:file', (req, res, go) => {
     const name = mediaPath + req.params.file
 
-    if (!filesystem.existsSync(name)) {
-        res.status(404)
-        res.sendFile(pagePath + '404media.html')
-
-        return
+    if (filesystem.existsSync(name)) {
+        res.sendFile(name)
     }
 
-    res.sendFile(name)
-    console.log(`[SUCCESS] Sent ${req.params.file} to ${req.ip}`)
+    else go
 });
+
+app.use((_, res) => {
+    res.send(pagePath + 'home.html')
+})
 
 app.listen(port, () => {
     console.log(`[SERVER] Listening on port ${port}`)
