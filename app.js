bf1d064e54034e619a14a8ce64137b892b9a9006
CozzyBro2 <gojinhan2@gmail.com>
2023-06-19 16:01:46 -0400

    polish

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
-
 const fs = require('fs')
 const path = require('path')
 
-const port = ( process.env.PORT || 5530 )
+const app = require('express')()
+const bodyParser = require('body-parser')
 
+const app_port = ( process.env.PORT || 5530 )
 const media_path = path.join(__dirname, '/media/')
 const page_path = path.join(__dirname, '/pages/')
 
-app.use(bodyParser.raw({type: "image/*", limit: "40mb"}))
-app.use(bodyParser.raw({type: "text/*", limit: "1gb"}))
-app.use(bodyParser.raw({type: "video/*", limit: "1gb"}))
-
-app.get('/', (_, res) => {
+function getHomepage(_, res) {
     res.sendFile(page_path + 'home.html')
-})
+}
 
 function getFile(req, res, go) {
     const name = media_path + req.params.file
@@ -27,13 +22,18 @@ function getFile(req, res, go) {
     else go()
 }
 
-app.get('/:file', getFile)
-app.get('/nocache/:file', getFile)
-
-app.use((_, res) => {
+function getFailure(_, res) {
     res.sendFile(page_path + '404media.html')
-})
+}
 
-app.listen(port, () => {
-    console.log(`[SERVER] Listening on port ${port}`)
+app.get('/', getHomepage)
+app.get('/:file', getFile)
+app.use(getFailure)
+
+app.use(bodyParser.raw({type: "image/*", limit: "40mb"}))
+app.use(bodyParser.raw({type: "text/*", limit: "100mb"}))
+app.use(bodyParser.raw({type: "video/*", limit: "1gb"}))
+
+app.listen(app_port, () => {
+    console.log(`[SERVER] Listening on port ${app_port}`)
 })
